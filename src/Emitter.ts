79c41f60914a9c29b306b3957c9341cebe6e72ea
wyattis <wyattisrael@gmail.com>
2019-09-15 13:07:33 -0400

    Added Emitter class

---
+++ src/Emitter.ts
@@ -0,0 +1,106 @@
+type EventCallback = {
+  callback: any
+  context: any
+}
+export default class Emitter {
+  private eventCallbacks: {[key: string]: EventCallback[]} = {}
+
+  /**
+   * Register an event type by name. This method is called when the 'on' method is called if an event of that type has
+   * not been registered yet.
+   * @param eventName
+   */
+  register (eventName: string) {
+    if (this.eventCallbacks[eventName]) {
+      throw Error('An event with this name has already been registered: ' + eventName)
+    }
+    this.eventCallbacks[eventName] = []
+  }
+
+  /**
+   * Add an event callback with optional context
+   * @param eventName
+   * @param callback
+   * @param context
+   */
+  on (eventName: string, callback: Function, context?: object) {
+    if (!this.eventCallbacks[eventName]) {
+      this.register(eventName)
+    }
+    this.eventCallbacks[eventName].unshift({
+      callback: callback,
+      context: context
+    })
+  }
+
+  /**
+   * Remove an event callback by reference
+   * @param eventName
+   * @param callback
+   * @param force
+   */
+  off (eventName: string, callback: Function, force: boolean = false) {
+    if (!this.eventCallbacks[eventName]) {
+      if (force) return
+      throw Error('An event with this name has not been registered yet: ' + eventName)
+    }
+    let cbInd = this.eventCallbacks[eventName].findIndex(cb => cb.callback === callback)
+    this.eventCallbacks[eventName].splice(cbInd, 1)
+  }
+
+  /**
+   * Dispatch an event with eventName using the 'call' method
+   * @param eventName
+   * @param args
+   */
+  dispatch (eventName: string, ...args: any[]) {
+    if (this.eventCallbacks[eventName]) {
+      let i = this.eventCallbacks[eventName].length
+      while (i--) {
+        this.eventCallbacks[eventName][i].callback.call(this.eventCallbacks[eventName][i].context, ...args)
+      }
+    }
+  }
+
+  /**
+   * Dispatch an event with eventName using the 'apply' method
+   * @param eventName
+   * @param args
+   */
+  dispatchApply (eventName: string, args: any[] = []) {
+    if (this.eventCallbacks[eventName]) {
+      let i = this.eventCallbacks[eventName].length
+      while (i--) {
+        this.eventCallbacks[eventName][i].callback.apply(this.eventCallbacks[eventName][i].context, args)
+      }
+    }
+  }
+
+  /**
+   * Alias for the dispatch method
+   * @param eventName
+   * @param args
+   */
+  emit (eventName: string, ...args: any[]) {
+    this.dispatch(eventName, ...args)
+  }
+
+  /**
+   * Remove all listeners from this instance
+   */
+  removeListeners () {
+    this.eventCallbacks = {}
+  }
+
+  /**
+   * Getter to check if any listeners are present on this emitter
+   */
+  hasListeners (): boolean {
+    for (let key in this.eventCallbacks) {
+      if (this.eventCallbacks[key].length) {
+        return true
+      }
+    }
+    return false
+  }
+}
---
+++ test/emitter.spec.ts
@@ -0,0 +1,97 @@
+import Emitter from '../src/Emitter'
+import 'mocha'
+import { expect } from 'chai'
+
+describe('Emitter', () => {
+  it('should emit in the order that callbacks were appended', () => {
+    const emitter = new Emitter()
+    const results: number[] = []
+    emitter.on('test', () => {
+      results.push(0)
+    })
+    emitter.on('test', () => {
+      results.push(1)
+    })
+    emitter.on('test', () => {
+      results.push(2)
+    })
+    emitter.dispatch('test')
+    expect(results).to.deep.equal([0, 1, 2], 'Emitted order did not match the order of the callbacks')
+  })
+  it('should apply the correct context for events', () => {
+    const emitter = new Emitter()
+    let resA = null
+    let resB = null
+    let a = { name: 'a' }
+    let b = { name: 'b' }
+    emitter.on('test', function () {
+      resA = this.name
+    }, a)
+    emitter.on('test', function () {
+      resB = this.name
+    }, b)
+    emitter.dispatch('test')
+    expect(resA).to.equal(a.name, 'Context was not applied correctly for a')
+    expect(resB).to.equal(b.name, 'Context was not applied correctly for b')
+  })
+  it('should remove the callback correctly', () => {
+    const emitter = new Emitter()
+    let val = 0
+    function test () {
+      val++
+    }
+    emitter.on('test', test)
+    emitter.dispatch('test')
+    expect(val).to.equal(1, 'callback was not called correctly')
+    emitter.off('test', test)
+    emitter.emit('test')
+    expect(val).to.equal(1, 'callback was still called after being removed')
+  })
+  it('should fail to register the same event twice', () => {
+    const emitter = new Emitter()
+    function register () {
+      emitter.register('test')
+      emitter.register('test')
+    }
+    expect(register).to.throw(Error, 'An event with this name has already been registered', 'Incorrectly allowed the emitter to register the same function twice')
+  })
+  it('should fail to remove a callback from an unregistered event', () => {
+    const emitter = new Emitter()
+    expect(() => emitter.off('test', () => false)).to.throw(Error, 'An event with this name has not been registered yet', 'Incorrectly allowed the emitter to try to remove a function on an event that does not exist yet')
+    expect(() => emitter.off('test', () => false, true)).to.not.throw()
+  })
+  it('should pass the correct arguments using dispatch and dispatchApply', () => {
+    const emitter = new Emitter()
+    const arg1 = { a: 'wow' }
+    const arg2 = { b: 'amaze' }
+    emitter.on('test', function (tArg1: object, tArg2: object) {
+      expect(tArg1).to.equal(arg1, 'The first argument does not match')
+      expect(tArg2).to.equal(arg2, 'The second argument does not match')
+    })
+    emitter.dispatch('test', arg1, arg2)
+    emitter.dispatchApply('test', [arg1, arg2])
+  })
+  it('should remove all listeners', () => {
+    const emitter = new Emitter()
+    let v = true
+    emitter.on('wow', () => {
+      // if it runs more than once this assertion will fail
+      expect(true).to.eql(v)
+      v = false
+    })
+    emitter.dispatch('wow')
+    emitter.removeListeners()
+    emitter.dispatch('wow')
+  })
+  it('should know about the listeners it has', () => {
+    const em = new Emitter()
+    expect(em.hasListeners()).to.eql(false)
+    em.on('ok', () => false)
+    expect(em.hasListeners()).to.eql(true)
+  })
+  it('should not fail to dispatch when no event is registered', () => {
+    const em = new Emitter()
+    expect(() => em.dispatch('test')).to.not.throw()
+    expect(() => em.dispatchApply('test')).to.not.throw()
+  })
+})
